1d33ef92ec3273a75f00c849b15c91faf3650cdc
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-07-19 16:14:41 +0800

    Update styles

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -2,7 +2,7 @@ function WeatherDisplay({ data }) {
 
     return (
         <>
-            <section className='weather-data'>
+            <section>
                 <img src={data.current.condition.icon} alt={data.current.condition.text} />
                 <h1>{data.location.name}</h1>
                 <p>{data.current.condition.text}</p>
